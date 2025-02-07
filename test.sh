652b5cf3ba6895579d7b0e169bef1b9d6147787f
Bee Wilkerson <bee.wilkerson@ymail.com>
2025-02-06 22:31:29 -0800

    Update test.sh comment

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ pytest -v tests/integration_tests/
 echo "\n Running e2e tests.  This may take a few minutes...\n"
 pytest -v tests/e2e_tests/
 
-# field tests are not run on the target machine where we can install
+# Field tests are typically run on the target machine where we can install
 # platform specific dependencies
 # echo "\n Running field tests...\n"
 # pytest tests/field_tests/
