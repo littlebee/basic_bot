60e6bd4713d67293d5453e7227e772efaf4b641d
Bee <bee.wilkerson@ymail.com>
2025-03-11 08:20:03 -0700

    Add thumbnail when saving video
    When video is recorded on request (earlier PR), also save a thumnail of the first frame for UI display.
    
    Also, don't fail in build.sh when npm doesn't exist.  test_vision.sh itegration test does not run on MacOS due to lack of tensorflow lite support.  In order to run the vision itegration test, you either need to push a PR (does run on CI/CD) or preemptively, upload the code to linux machine - like the onboard Raspberry Pi and run the test there, which is what I did.
    
    I also manually tested the .jpg image saved is visually correct.   You can use a simple GET request in a browser,  or  to http://pi5.local:5801/record_video
    and then  the recorded files locally.

--- build.sh
+++ build.sh
@@ -4,12 +4,14 @@
 # the files that `bb_create` creates.
 
 
-# fail on any error
-set -e
-
+# this can fail on linux or if running on a machine without a node
+# installed
 echo "Installing npm dependencies locally..."
 cd src/basic_bot/created_files/webapp/ && npm install
 cd ../../../..
+
+# fail on any error
+set -e
 
 echo "Linting..."
 python -m flake8 src/basic_bot
